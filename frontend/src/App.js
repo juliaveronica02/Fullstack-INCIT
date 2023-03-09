598c021d4666ddc3866f4e96caa6fd773548a5b9
juliaveronica02 <juliavero2206@gmail.com>
2023-03-09 10:19:25 +0700

    change app.js file routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/shared/layout";
 import Dashboard from "./pages/dashboard"
-import Product from "./components/product";
+import Product from "./components/approval";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path="product" element={<Product />} />
+          <Route path="approvals" element={<Product />} />
         </Route>
         <Route path="login" element={<div>this is login page</div>} />
       </Routes>
